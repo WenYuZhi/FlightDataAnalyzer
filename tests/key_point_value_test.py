a305252df3ebfac9258553c1abb7fbefb51fd095
Daniel Gillet <daniel.gillet@flightdataservices.com>
2020-01-28 08:10:38 +0000

    AE-2785 Use Mock for aircrafttables accessor

--- tests/key_point_value_test.py
+++ tests/key_point_value_test.py
@@ -16444,18 +16444,22 @@ class TestFuelQtyWingDifferenceOverThresholdMax(unittest.TestCase):
                 None)
         )
 
-    def test_can_operate_unknown_model_logs_warning(self):
+    @patch('analysis_engine.key_point_values.at')
+    def test_can_operate_unknown_model_logs_warning(self, at):
+        at.get_fuel_imbalance_limits.side_effect = KeyError('Error Message')
         with self.assertLogs(None, level='WARNING') as cm:
             self.node_class.can_operate(
                 [('Fuel Qty (L)', 'Fuel Qty (R)', 'Airborne', 'Family')],
                 A('Family', value='B707'))
         expected = (
         "WARNING:analysis_engine.key_point_values.FuelQtyWingDifferenceOverThresholdMax:"
-        "\"No fuel imbalance limit for model 'None', series 'None', family 'B707'.\""
+        "'Error Message'"
         )
         self.assertEqual(cm.output, [expected])
 
-    def test_low_imbal_low_total_fuel(self):
+    @patch('analysis_engine.key_point_values.at')
+    def test_low_imbal_low_total_fuel(self, at):
+        at.get_fuel_imbalance_limits.return_value = ((21_772, 36_197), (1_134, 680))
         left_wing = P('Fuel Qty (L)', array=np.ma.ones(10) * 10_000, frequency=1/4)
         right_wing = P('Fuel Qty (R)', array=np.ma.ones(10) * (10_000 - 1_133), frequency=1/4)
         airbornes = buildsection('Airborne', 0, 10)
@@ -16466,7 +16470,9 @@ class TestFuelQtyWingDifferenceOverThresholdMax(unittest.TestCase):
 
         self.assertEqual(len(node), 0)
 
-    def test_high_imbal_low_total_fuel(self):
+    @patch('analysis_engine.key_point_values.at')
+    def test_high_imbal_low_total_fuel(self, at):
+        at.get_fuel_imbalance_limits.return_value = ((21_772, 36_197), (1_134, 680))
         left_wing = P('Fuel Qty (L)', array=np.ma.ones(10) * 10_000, frequency=1/4)
         right_wing = P('Fuel Qty (R)', array=np.ma.ones(10) * (10_000 - 2_000), frequency=1/4)
         airbornes = buildsection('Airborne', 0, 10)
@@ -16479,7 +16485,9 @@ class TestFuelQtyWingDifferenceOverThresholdMax(unittest.TestCase):
         self.assertEqual(node[0].index, 0)
         self.assertEqual(node[0].value, -2_000)
 
-    def test_low_imbal_high_total_fuel(self):
+    @patch('analysis_engine.key_point_values.at')
+    def test_low_imbal_high_total_fuel(self, at):
+        at.get_fuel_imbalance_limits.return_value = ((21_772, 36_197), (1_134, 680))
         left_wing = P('Fuel Qty (L)', array=np.ma.ones(10) * 18_500, frequency=1/4)
         right_wing = P('Fuel Qty (R)', array=np.ma.ones(10) * (18_500 - 679), frequency=1/4)
         airbornes = buildsection('Airborne', 0, 10)
@@ -16490,7 +16498,9 @@ class TestFuelQtyWingDifferenceOverThresholdMax(unittest.TestCase):
 
         self.assertEqual(len(node), 0)
 
-    def test_high_imbal_high_total_fuel(self):
+    @patch('analysis_engine.key_point_values.at')
+    def test_high_imbal_high_total_fuel(self, at):
+        at.get_fuel_imbalance_limits.return_value = ((21_772, 36_197), (1_134, 680))
         left_wing = P('Fuel Qty (L)', array=np.ma.ones(10) * 18_500, frequency=1/4)
         right_wing = P('Fuel Qty (R)', array=np.ma.ones(10) * (18_500 - 800), frequency=1/4)
         airbornes = buildsection('Airborne', 0, 10)
@@ -16503,7 +16513,9 @@ class TestFuelQtyWingDifferenceOverThresholdMax(unittest.TestCase):
         self.assertEqual(node[0].index, 0)
         self.assertEqual(node[0].value, -800)
 
-    def test_high_imbal_interpolation(self):
+    @patch('analysis_engine.key_point_values.at')
+    def test_high_imbal_interpolation(self, at):
+        at.get_fuel_imbalance_limits.return_value = ((21_772, 36_197), (1_134, 680))
         left_wing = P('Fuel Qty (L)', array=np.ma.ones(10) * 15_500 - 1_000, frequency=1/64)
         right_wing = P('Fuel Qty (R)', array=np.ma.ones(10) * 15_500, frequency=1/64)
         airbornes = buildsection('Airborne', 0, 10)
